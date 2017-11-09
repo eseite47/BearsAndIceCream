54aac8b131cf2be41496c72f12e620e7d87fbf3f
Elisabeth Seite <elisabeth.seite@gmail.com>
2017-11-09 14:14:31 -0500

    real changes

--- bears/bear.js
+++ bears/bear.js
@@ -2,5 +2,12 @@
 // I am starting my project
 
 function iceCreamIsBetterThanBears(flavor) {
-    console.log("mwahahaha");
+    console.log('mwahahaha');
+}
+
+//what is this?
+//I think we should work better with Git
+
+function whatTimeIsIt() {
+    //I will work this later
 }
